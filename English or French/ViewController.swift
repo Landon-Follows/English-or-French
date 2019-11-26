7294f310b44fba24634781a0da48fdd40362324e
Landon Follows <lfollows@lcs.on.ca>
2019-11-26 11:13:05 -0500

    Created Guard Statement and Visual Layout for French & English

--- English or French/ViewController.swift
+++ English or French/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var uiLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    //MARK: Properties
+    
+    @IBAction func analyze(_ sender: Any) {
+        
+        //Guard Statement When user doesn't input any text
+        
+        guard let imputTextAString = textView.text, imputTextAString != "" else{
+                
+            uiLabel.text = "Please Type some English or French"
+                
+            return
+        }
+        
+        
+        
+    }
+    
+    
 }
-
